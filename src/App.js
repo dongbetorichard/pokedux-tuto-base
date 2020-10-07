445ad9ca4b32c97746045f59759d4d4f1be4fc8e
dongbetorichard <donberi77@gmail.com>
2020-10-07 20:32:55 +0200

    mon premier commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+import { connect } from 'react-redux'
+
+import GameBoy from "./components/GameBoy";
+import PokeList from "./components/PokeList";
+
+const App = ({ click }) => {
+  return (
+    <div className="App">
+      {click}
+      <GameBoy />
+      <PokeList />
+    </div>
+  );
+};
+
+const mapStateToProps = ({ click }) => {
+  return {
+    click
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/store/reducer.js
@@ -0,0 +1,6 @@
+import initialState from "./initialState";
+
+const reducer = (state = initialState) => {
+    return State;
+};
+export default reducer
